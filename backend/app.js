fab4554e97c132ef532cb4864a7cd3bd7c45e78b
Garvit <garvitg0108@gmail.com>
2024-08-13 00:34:32 +0530

    Final changes

--- backend/app.js
+++ backend/app.js
@@ -6,17 +6,19 @@ const app = express();
 const cors = require('cors');
 app.use(express.json())
 app.use(cors());
+
 const adminRotuer = require('./routes/admin')
 const userRotuer = require('./routes/user')
 
 app.use('/api/admin',adminRotuer);
 app.use('/api/user',userRotuer);
 
+const port = process.env.PORT || 3000;
 
 app.get('/test',(req,res)=>{
   res.send("hello buddy")
 })
 
-app.listen(3000,()=>{
-  console.log("Server running on port 3000")
+app.listen(port,()=>{
+  console.log("Server running on port ",port)
 })
